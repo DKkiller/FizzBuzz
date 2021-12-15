927b97ed2b8760449f6cc15811c7ecdf6ec2649d
zhihan.zhu <zhihan.zhu@thoughtworks.com>
2021-12-15 14:46:56 +0800

    [zhihan.zhu] Fix: fix the bugs.

--- src/main/java/com/tw/practice/rule/IRuleImplContains5.java
+++ src/main/java/com/tw/practice/rule/IRuleImplContains5.java
@@ -1,6 +1,12 @@
 package com.tw.practice.rule;
 
 public class IRuleImplContains5 implements IRule {
+    private IRule ruleMultiples7;
+
+    public IRuleImplContains5() {
+        this.ruleMultiples7 = new IRuleImplMultiples7();
+    }
+
     @Override
     public boolean match(String position) {
         return position.contains("5");
@@ -8,8 +14,10 @@ public class IRuleImplContains5 implements IRule {
 
     @Override
     public String say(String position) {
-        if (match(position))
-            return "Buzz";
+        if (match(position) && ruleMultiples7.match(position))
+            return "BuzzWhizz";
+        else if (match(position) && !ruleMultiples7.match(position))
+            return "BuzzWhizz";
         else
             return position;
 
