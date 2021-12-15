dbd68e0b12a594518847ca003712923842dfcefb
zhihan.zhu <zhihan.zhu@thoughtworks.com>
2021-12-15 14:55:34 +0800

    [zhihan.zhu] Fix: fix the bugs.

--- src/main/java/com/tw/practice/rule/IRuleImplContains5.java
+++ src/main/java/com/tw/practice/rule/IRuleImplContains5.java
@@ -1,9 +1,11 @@
 package com.tw.practice.rule;
 
 public class IRuleImplContains5 implements IRule {
+    private IRule ruleMultiples5;
     private IRule ruleMultiples7;
 
     public IRuleImplContains5() {
+        this.ruleMultiples5 = new IRuleImplMultiples5();
         this.ruleMultiples7 = new IRuleImplMultiples7();
     }
 
@@ -14,10 +16,12 @@ public class IRuleImplContains5 implements IRule {
 
     @Override
     public String say(String position) {
-        if (match(position) && ruleMultiples7.match(position))
-            return "BuzzWhizz";
-        else if (match(position) && !ruleMultiples7.match(position))
-            return "BuzzWhizz";
+        if (ruleMultiples5.match(position) && ruleMultiples7.match(position))
+            return ruleMultiples5.say(position) + ruleMultiples7.say(position);
+        else if (ruleMultiples5.match(position) && !ruleMultiples7.match(position))
+            return ruleMultiples5.say(position);
+        else if (!ruleMultiples5.match(position) && ruleMultiples7.match(position))
+            return ruleMultiples7.say(position);
         else
             return position;
 
